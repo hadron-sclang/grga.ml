e935cd1520dac209a5f0b6c6c86ad2c24c32822a
Luke Nihlen <luke.nihlen@gmail.com>
2022-10-19 23:05:24 -0400

    more ui wip

--- src/main.ts
+++ src/main.ts
@@ -3,41 +3,20 @@ import 'monaco-editor/esm/vs/editor/editor.all.js';
 
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
-/*
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
-*/
-
-/*
-monaco.editor.create(document.getElementById('app'), {
-  value: "function hello() {\n\talert('Hello world!');\n}",
-  language: 'javascript'
-});
-*/
-
-monaco.editor.create(document.getElementById('container'), {
+monaco.editor.create(document.getElementById('container')!, {
   value: "Paste your SuperCollider code here",
   theme: 'vs-dark',
+  automaticLayout: true
 });
 
-/*
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+function setupCounter(element: HTMLButtonElement) {
+  let counter = 0
+  const setCounter = (count: number) => {
+    counter = count
+    element.innerHTML = `count is ${counter}`
+    }
+  element.addEventListener('click', () => setCounter(++counter))
+  setCounter(0)
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
-*/
+setupCounter(document.querySelector<HTMLButtonElement>('#formatButton')!);
